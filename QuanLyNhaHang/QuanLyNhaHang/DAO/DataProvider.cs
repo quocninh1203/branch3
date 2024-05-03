d48aebdd56b69a7d318cfceddb714afaffc17dbf
quocninh <quocninh9.32017@gmail.com>
2024-05-03 17:23:16 +0700

    lam lai bai 5 by vinh

---
+++ QuanLyNhaHang/QuanLyNhaHang/DAO/DataProvider.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance; // Ctrl + R +E
+        public static DataProvider Instance 
+        {
+            get { if (instance == null) instance = new DataProvider();
+                return DataProvider.instance; }
+            private set { DataProvider.instance = value; }
+        }
+
+        private DataProvider() { }
+
+        private string chuoiketnoi = "Data Source = YOUNGTEE; Initial Catalog = QuanLyNhaHang; Integrated Security = True";
+       
+        // data source đỏi lại cho đúng tùy máy"
+        // vinh: LAPTOP-HRCJET3B
+        // minh: YOUNGTEE
+        // ninh: LAPTOP-MHOHQ41L\\SQLEXPRESS
+        public DataTable ExecuteSQL(string sql, object[] parameter = null) // có thể bằng null
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection ketnoi = new SqlConnection(chuoiketnoi)) // tự giải phóng bộ nhớ
+            {
+                ketnoi.Open();
+                SqlCommand lenh = new SqlCommand(sql, ketnoi);
+                if ( parameter != null)
+                {
+                    string[] listPara = sql.Split(' '); // split theo khoảng trắng
+                    int i = 0;
+                    foreach (string  item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            lenh.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(lenh);
+                adapter.Fill(data);
+                ketnoi.Close();
+            }
+            return data;
+        }
+        // số dòng execute thành công
+        public int ExecuteNonSQL(string sql, object[] parameter = null) // có thể bằng null
+        {
+            int data = 0;
+            using (SqlConnection ketnoi = new SqlConnection(chuoiketnoi)) // tự giải phóng bộ nhớ
+            {
+                ketnoi.Open();
+                SqlCommand lenh = new SqlCommand(sql, ketnoi);
+                if (parameter != null)
+                {
+                    string[] listPara = sql.Split(' '); // split theo khoảng trắng
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            lenh.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data= lenh.ExecuteNonQuery();
+                ketnoi.Close();
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string sql, object[] parameter = null) // có thể bằng null
+        {
+            object data = 0;
+            using (SqlConnection ketnoi = new SqlConnection(chuoiketnoi)) // tự giải phóng bộ nhớ
+            {
+                ketnoi.Open();
+                SqlCommand lenh = new SqlCommand(sql, ketnoi);
+                if (parameter != null)
+                {
+                    string[] listPara = sql.Split(' '); // split theo khoảng trắng
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            lenh.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = lenh.ExecuteScalar();
+                ketnoi.Close();
+            }
+            return data;
+        }
+    }
+}
---
+++ QuanLyNhaHang/QuanLyNhaHang/fTableManager.cs
@@ -0,0 +1,150 @@
+﻿using QuanLyNhaHang.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace QuanLyNhaHang
+{
+    public partial class fTableManager : Form
+    {
+        
+
+        
+
+        public fTableManager()
+        {
+            
+        }
+        void load()
+        {
+           
+        }
+        #region Method
+        void ChangeAccount(int type)
+        {
+           
+        }
+        void LoadCategory()
+        {
+            
+        }
+        void LoadFoodListByCategoryID(int id)
+        {
+           
+        }
+        void LoadTable()
+        {
+           
+        }
+        void ShowBill(int id)
+        {
+            
+        }
+        void LoadComboboxTable(ComboBox cb)
+        {
+           
+        }
+        #endregion
+
+        #region Event
+        int idhientai = 0;
+        void btn_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void adminToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void F_DeleteFood(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void F_UpdateFood(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void F_InsertFood(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void f_UpdateAccount(object sender)
+        {
+            
+        }
+
+        private void cbCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnAddFood_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnCheckOut_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnSwitchTable_Click(object sender, EventArgs e)
+        { 
+          
+        }
+
+        #endregion
+
+        private void rf_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBill_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void In_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void fTableManager_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
